dbdef65af5ca74416816aa9f224db3ef14844993
tojik_proof_93 <elementman50@yandex.com>
2026-07-12 18:35:52 +0500

    Create OFFSETNEW.cs

---
+++ Download/OFFSETNEW.cs
@@ -0,0 +1,57 @@
+
+Custom Patch for Cover Fire: Offline Shooting
+
+App Name: Cover Fire: Offline Shooting
+Package Name: com.generagames.resistance
+Version: 1.24.09
+Build: 299
+
+Play Store Link:
+https://play.google.com/store/apps/details?id=com.generagames.resistance
+
+Patch Info:
+👉 VIP Level 5 Activated
+👉 Unlimited Cash
+👉 Unlimited Gold
+👉 Unlimited Energy
+👉 Unlimited Battle Tickets
+👉 Unlimited Event Tickets
+👉 Instant Reload
+👉 Unlimited Health
+
+Methods to Patch:
+
+👉 For Activating VIP
+class VIPUtils
+int GetVIPLevel()
+
+class Mercenarios
+👉 For Unlimited Cash
+int monedas()
+👉 For Unlimited Gold
+int gold()
+👉 For Unlimited Energy
+int get_player_energy()
+👉 For Unlimited Battle Tickets
+int get_player_tickets()
+👉 For Unlimited Event Tickets
+int get_player_eventtickets()
+
+👉 For Instant Reload
+class PlayerWeapon
+float CalculateReloadTime()
+
+👉 For Unlimited Health
+class PlayerControl
+
+void ApplyDamage(float damage, Vector3 direccionDamage, Vector3 puntoImpactoFinal, Vector3 direccionHitNormal, GameObject objcreater, PlayerControl.origenDamage _origenDamage, GameObject obj_origen)
+
+👉 For No Direct Damage
+
+void ApplyDirectDamage(float damage, Vector3 direccionDamage, Vector3 puntoImpactoFinal, PlayerControl.origenDamage _origenDamage)
+━━━━━━━━━━━━━━━━━━━
+#il2cpp #libPatching #Patch #LuckyPatcher
+💥💥💥💥💥💥💥💥💥💥💥
+💡 Patch by @TDOhex
+♻️ Join us for more Info
+💥💥💥💥💥💥💥💥💥💥💥
